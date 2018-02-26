8542fd683c3d20f9407ef911702c21161d96ee77
Vladlen <shmelev.dev@gmail.com>
2018-02-26 13:11:58 +1000

    Add user and picture models

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class Exposition(models.Model):
+    user = models.ForeignKey(User, null=True)
     name = models.CharField(max_length=200, verbose_name='Название выставки')
 
+
+class Picture(models.Model):
+    exposition = models.ForeignKey(Exposition, null=True)
+    picture = models.ImageField(upload_to='pictures')
