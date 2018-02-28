b2eddc3720d94290ac7ca3e44f0e0172f8615ff9
Vladlen <shmelev.dev@gmail.com>
2018-02-28 15:09:47 +1000

    Add picture serializer

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers, fields
 
-from backend.models import Exposition
+from backend.models import Exposition, Picture
 
 
 class ExpositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exposition
         fields = '__all__'
+
+
+class PictureSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Picture
+        fields = ('id', 'image_url')
+
+    def get_image_url(self, picture):
+        request = self.context.get('request')
+        image_url = picture.image.url
+        return request.build_absolute_uri(image_url)
