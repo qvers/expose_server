d0d27ee6780a526a7063135251bb9866e9d0a790
Vladlen <shmelev.dev@gmail.com>
2018-02-28 15:11:28 +1000

    Modify urls

--- expose_server/urls.py
+++ expose_server/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^back/api/', include('backend.urls', namespace='api'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
